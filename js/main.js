816d1c9a5336444517a6cb1eb536739e9277cf5e
Argge <argge.yt@gmail.com>
2025-04-01 11:51:08 +0200

    Mini Update

--- js/main.js
+++ js/main.js
@@ -1,25 +1,38 @@
-let number = document.querySelectorAll("number");
+let number = document.querySelectorAll(".number");
 let symbol = document.getElementsByClassName("symbol");
 let symbolMain = document.getElementsByClassName("symbolMain");
 let randomBtn = document.getElementById("randomBtn");
 
-randomBtn.addEventListener("click", () => {
-    let number1 = document.getElementById("number1");
-    let NumCard = randomNum();
-    // let ColorCard = randomColorSymbol();
-    // let Symbol = randomColorSymbol();
-    if (NumCard === 1) {
+// randomBtn.addEventListener("click", () => {
+//     let number1 = document.getElementById("number1");
+//     let NumCard = randomNum();
+//     let ColorCard = randomColorSymbol();
+//     let Symbol = randomColorSymbol();
+//     if (NumCard === 1) { 
         
-    }
-    number.textContent = "1";
+//     }
+//     number.textContent = "1";
+// });
+
+number.forEach(function(num) {
+    randomBtn.addEventListener("click", () => {
+        // let NumCard = randomNum1();
+        num.textContent = 12; 
+    });
 });
 
 function randomNum() {
-    let num = Math.floor((Math.random()*(14-1)) + 1);
-    console.log(num);
+    let indexNum = Math.floor((Math.random()*(14-1)) + 1);
+    console.log(indexNum);
 }
 
 function randomColorSymbol() {
-    let index = Math.floor((Math.random()*(4-1)) + 1);
-    console.log(index);
+    let indexCol = Math.floor((Math.random()*(4-1)) + 1);
+    console.log(indexCol);
+}
+
+// test function witch value = 1
+function randomNum1() {
+    let indexNum = Math.floor((Math.random()*(1-1)) + 1);
+    console.log(indexNum);
 }
