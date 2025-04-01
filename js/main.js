344de590453402ca88b025fcdfade5ae0eb7cbbd
Argge <argge.yt@gmail.com>
2025-04-01 13:32:13 +0200

    Mini Fix

--- js/main.js
+++ js/main.js
@@ -16,23 +16,32 @@ let randomBtn = document.getElementById("randomBtn");
 
 number.forEach(function(num) {
     randomBtn.addEventListener("click", () => {
-        // let NumCard = randomNum1();
-        num.textContent = 12; 
+        let NumCard = randomNum();
+        num.textContent = NumCard;
+        let ColCard = randomColorSymbol();
+        
     });
 });
 
 function randomNum() {
     let indexNum = Math.floor((Math.random()*(14-1)) + 1);
     console.log(indexNum);
+    // let text = String.fromCharCode(indexNum);
+    let text = indexNum.toString();
+    return text;
 }
 
 function randomColorSymbol() {
     let indexCol = Math.floor((Math.random()*(4-1)) + 1);
     console.log(indexCol);
+    let text = indexCol.toString();
+    return text;
 }
 
 // test function witch value = 1
 function randomNum1() {
     let indexNum = Math.floor((Math.random()*(1-1)) + 1);
     console.log(indexNum);
+    let text = indexNum.toString();
+    return text;
 }
