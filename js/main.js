5c42d6aae1470be462fb5bb4df12a447f46ac3b8
Argge <argge.yt@gmail.com>
2025-04-01 11:13:42 +0200

    Fix

--- js/main.js
+++ js/main.js
@@ -1,32 +1,25 @@
-let chekBtn = document.getElementById("chekBtn");
-let text = document.getElementById("textInput");
-let resultat = document.getElementById("res");
-let resultat1 = document.getElementById("res1");
-let cats = document.querySelectorAll(".cats");
+let number = document.querySelectorAll("number");
+let symbol = document.getElementsByClassName("symbol");
+let symbolMain = document.getElementsByClassName("symbolMain");
+let randomBtn = document.getElementById("randomBtn");
 
-const min = 1;
-const max = 10;
-// const nbr = Math.floor(Math.random() * (max - min)) + min;
-// const nbr = function Random() {
-//     return Math.floor(Math.random() * (max - min)) + min;
-// }
-let nbr;
-
-
-chekBtn.addEventListener("click", () => { 
-    nbr = Math.floor(Math.random() * (max - min)) + min;
-    resultat1.innerHTML = nbr;
-    console.log(nbr);
-    if (text.value == nbr) {
-        resultat.innerHTML = "Nice!";
-        cats.forEach ((cat) => {
-            cat.style.display = "flex";
-        }); 
+randomBtn.addEventListener("click", () => {
+    let number1 = document.getElementById("number1");
+    let NumCard = randomNum();
+    // let ColorCard = randomColorSymbol();
+    // let Symbol = randomColorSymbol();
+    if (NumCard === 1) {
+        
     }
-    else {
-        resultat.innerHTML = "Womp-womp";
-        cats.forEach ((cat) => {
-            cat.style.display = "none";
-        }); 
-    }
+    number.textContent = "1";
 });
+
+function randomNum() {
+    let num = Math.floor((Math.random()*(14-1)) + 1);
+    console.log(num);
+}
+
+function randomColorSymbol() {
+    let index = Math.floor((Math.random()*(4-1)) + 1);
+    console.log(index);
+}
