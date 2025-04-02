bc50404dc54c51e31911a295e23d986e98eb9c06
Argge <argge.yt@gmail.com>
2025-04-02 11:42:01 +0200

    Big Update
    Now random number generater for card is working. Random color generator is in process

--- js/main.js
+++ js/main.js
@@ -1,47 +1,38 @@
-let number = document.querySelectorAll(".number");
+let number = document.getElementsByClassName("number");
 let symbol = document.getElementsByClassName("symbol");
 let symbolMain = document.getElementsByClassName("symbolMain");
 let randomBtn = document.getElementById("randomBtn");
 
-// randomBtn.addEventListener("click", () => {
-//     let number1 = document.getElementById("number1");
-//     let NumCard = randomNum();
-//     let ColorCard = randomColorSymbol();
-//     let Symbol = randomColorSymbol();
-//     if (NumCard === 1) { 
-        
-//     }
-//     number.textContent = "1";
-// });
-
-number.forEach(function(num) {
-    randomBtn.addEventListener("click", () => {
-        let NumCard = randomNum();
-        num.textContent = NumCard;
-        let ColCard = randomColorSymbol();
-        
-    });
+randomBtn.addEventListener("click", () => {
+    let NumCard = randomNum();
+    let ColCard = randomColor();
+    for (let i = 0; i < number.length; i++) {
+        number[i].textContent = NumCard;
+    }
+    if (ColCard === 1) {
+        for (let i = 0; i < number.length; i++) {
+            number[i].classList.remove("colorRed");
+        }    
+    }
 });
 
 function randomNum() {
     let indexNum = Math.floor((Math.random()*(14-1)) + 1);
     console.log(indexNum);
-    // let text = String.fromCharCode(indexNum);
     let text = indexNum.toString();
     return text;
 }
 
-function randomColorSymbol() {
-    let indexCol = Math.floor((Math.random()*(4-1)) + 1);
+function randomSymbol() {
+    let indexSym = Math.floor((Math.random()*(4-1)) + 1);
+    console.log(indexSym);
+    let text = indexSym.toString();
+    return text;
+}
+
+function randomColor() {
+    let indexCol = Math.floor((Math.random()*(3-1)) + 1);
     console.log(indexCol);
     let text = indexCol.toString();
-    return text;
-}
-
-// test function witch value = 1
-function randomNum1() {
-    let indexNum = Math.floor((Math.random()*(1-1)) + 1);
-    console.log(indexNum);
-    let text = indexNum.toString();
     return text;
 }
